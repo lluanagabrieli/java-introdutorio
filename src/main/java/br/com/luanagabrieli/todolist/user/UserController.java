c08960dcb69693a5e442aa84c280f397c7e6cb84
lluanagabrieli <luanagabrieli4599@hotmail.com>
2026-08-04 21:52:47 -0300

    feat: funcionalidade de criar user

---
+++ src/main/java/br/com/luanagabrieli/todolist/user/UserController.java
@@ -0,0 +1,19 @@
+package br.com.luanagabrieli.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// O nome da classe precisa ser o mesmo do arquivo
+// Modificadores de acesso: public, private, protected
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/create")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.username);
+    }
+}
---
+++ src/main/java/br/com/luanagabrieli/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.luanagabrieli.todolist.user;
+
+public class UserModel {
+    // Quando eu não coloco o modificador, ele é considerado public, mas só é acessível dentro do mesmo pacote
+    public String username;
+    public String name;
+    public String password;
+}
