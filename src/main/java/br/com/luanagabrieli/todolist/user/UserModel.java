12dab086dc32815548ff14cc3be4299ef22e4390
lluanagabrieli <luanagabrieli4599@hotmail.com>
2026-08-04 22:20:29 -0300

    feat: adição da lib Lombok para código mais limpo

--- src/main/java/br/com/luanagabrieli/todolist/user/UserModel.java
+++ src/main/java/br/com/luanagabrieli/todolist/user/UserModel.java
@@ -1,34 +1,16 @@
 package br.com.luanagabrieli.todolist.user;
 
+import lombok.Data;
+
+// Insere getters automaticamente para todos os atributos
+//@Getter
+
+// Insere getters e setters automaticamente para todos os atributos, não precisa mais escrever manualmente
+@Data
 public class UserModel {
     // Quando eu não coloco o modificador, ele é considerado public, mas só é acessível dentro do mesmo pacote
     //quanto é private, só é acessível dentro da própria classe
     private String username;
     private String name;
     private String password;
-
-    // Getters e Setters
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
