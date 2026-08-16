162ed7297aa128d84e22343f0ae4f799f23fb62e
lluanagabrieli <luanagabrieli4599@hotmail.com>
2026-08-16 19:17:14 -0300

    feat: dependência do bcrypt e hash da senha

--- src/main/java/br/com/luanagabrieli/todolist/user/UserController.java
+++ src/main/java/br/com/luanagabrieli/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -26,6 +28,10 @@ public class UserController {
             // dentro do status pode passar o código diretamente ou utilizar o HttpStatus. Se clicar nele é possível consultar os status disponíveis
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        // Hash da senha utilizando a biblioteca BCrypt
+        var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordHashed);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.OK).body(userCreated);
